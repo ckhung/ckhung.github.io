80881f81c53811e47c4492f6c7825467251eb367
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2016-12-12 15:19:24 +0800

    fix img right align

--- a/lib/blogger.js
+++ a/lib/blogger.js
@@ -1,6 +1,9 @@
+/* global $, document */
 $(document).ready(function(){
   $('img').each(function() {
-    $(this).wrap('<a style="float:right" href="' + $(this).attr('src') + '" title="' + $(this).attr('alt') + '">').parent().zoom().fancybox({
+    if ($(this).hasClass('nozoom')) { return; }
+    align = $(this).attr('align') ? 'float:right' : '';
+    $(this).wrap('<a style="' + align + '" href="' + $(this).attr('src') + '" title="' + $(this).attr('alt') + '">').parent().zoom().fancybox({
 	openEffect	: 'elastic',
 	closeEffect	: 'elastic'
     });
