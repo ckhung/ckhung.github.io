498868b8c3e59169c5b151da7b40978e8c2e7a01
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2016-12-13 17:28:24 +0800

    fix align:left

--- a/lib/blogger.js
+++ a/lib/blogger.js
@@ -2,7 +2,8 @@
 $(document).ready(function(){
   $('img').each(function() {
     if ($(this).hasClass('nozoom')) { return; }
-    align = $(this).attr('align') ? 'float:right' : '';
+    align = $(this).attr('align');
+    align = align ? 'float:' + align : '';
     $(this).wrap('<a style="' + align + '" href="' + $(this).attr('src') + '" title="' + $(this).attr('alt') + '">').parent().zoom().fancybox({
 	openEffect	: 'elastic',
 	closeEffect	: 'elastic'
