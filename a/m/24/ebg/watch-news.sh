744d49922f90fff576deae8cf4f49e4f62219559
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2024-05-14 09:35:39 +0800

    vlc -f 全螢幕； 改成 cut 處理任意欄位

--- a/m/24/ebg/watch-news.sh
+++ a/m/24/ebg/watch-news.sh
@@ -1,11 +1,11 @@
 source easybashgui
 
 while true ;  do
-    menu $( perl -pe 's/.*?,(.*)/"#$.# $1"/' news-channels.csv )
-    ans=$( cat ${dir_tmp}/${file_tmp})
+    menu $( perl -pe 's/.*?,(.*)/"#$.#,$1"/' news-channels.csv )
+    ans=$( cat ${dir_tmp}/${file_tmp} )
     if [ -z "$ans" ]; then break ; fi
-    ans_i=$( echo $ans | perl -ne 'print("$1") if m/#(\d+)#/' )
-    url=$( head -n $ans_i news-channels.csv | tail -n 1 | grep -Po 'http[^,]*' )
+    ans_i=$( echo $ans | perl -ne 'print("$1") if m/#(\d+)#,/' )
+    url=$( head -n $ans_i news-channels.csv | tail -n 1 | cut -d, -f 1 )
     read -t 3 -p "$ans => $ans_i => $url" ; echo ''
-    # vlc $url
+    # vlc -f $url
 done
